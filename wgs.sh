ef96b56c158c7eb1a38769785c522814619b485f
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-24 14:55:16 +0200

    convertion de sam to bam

--- wgs.sh
+++ wgs.sh
@@ -30,8 +30,45 @@ else
 fi
 
 # 1-a : Indexer la banque à l'aide de bowtie2-build
-./soft/bowtie2-build databases/all_genome.fasta results/rebuilt_genome/built_genome.fasta
+# Vérifier que le fichier a bien été créé.
+if [ -f "results/rebuilt_genome/built_genome.1.bt2" ]; then
+    echo "Le génome a été built."
+else
+    echo "Le génome va etre crée."
+    ./soft/bowtie2-build databases/all_genome.fasta results/rebuilt_genome/built_genome
+fi
 
-#./soft/bowtie2 -1 -2 -S results/sam_output/sam_out.sam
+# Alignement
+if [ -f "results/sam_output/sam_echG.sam" ]; then
+    echo "sam crée."
+else
+    echo "sam non crée."
+    ./soft/bowtie2 -p 6 --end-to-end -x results/rebuilt_genome/built_genome -1 fastq/EchG_R1.fastq -2 fastq/EchG_R2.fastq -S results/sam_output/sam_echG.sam
+fi
+#
+echo "Alignement est fini"
+# 2 - Calculer l'abondance de chaque bactérie en analysant le fichier sam.
+# 2 - a : Convertir votre fichier sam en bam (format binaire) (samtools view) + compression rapide
+
+# Make samtools si ce n'est pas fait.
+make soft/samtools-1.6/
+
+# Vérification du dossier sam_to_bam.
+if [ -d "results/sam_to_bam" ]; then
+    echo "Le dossier sam_to_bam a déjà été créé."
+else
+    mkdir results/sam_to_bam
+    echo "Le dossier sam_to_bam est créé."
+fi
+
+# Création du bam.
+if [ -f "results/sam_to_bam/output.bam" ]; then
+    echo "bam déjà crée."
+else
+    echo "bam non crée."
+    ./soft/samtools-1.6/samtools view -b --thread 6 -1 results/sam_output/sam_echG.sam -o results/sam_to_bam/output.bam
+    echo "Samtool view a créé le binaire."
+fi
+
 
 
