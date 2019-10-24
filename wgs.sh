b137c216bdfca4a39b8a3a18c1b4634b3ba59784
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-24 13:40:09 +0200

    Ajout d'un built sur le génome

--- wgs.sh
+++ wgs.sh
@@ -18,9 +18,8 @@ fi
 # Aligner les reads avec all_genome.fasta + end to end avec mode rapide.
 # retourner un fichier sam.
 
-./soft/bowtie2 --help
-
-if [ -d "result" ] && [] && ; then
+# Verifie que les bons dossiers sont créés.
+if [ -d "results" ] && [ -d "results/rebuilt_genome" ] && [ -d "results/sam_output" ]; then
     echo "Le dossier results a été créé."
     echo "Le dossier rebuilt_genome a été créé."
     echo "le dossier sam_output a été crée."
@@ -29,6 +28,9 @@ else
     mkdir results/rebuilt_genome
     mkdir results/sam_output
 fi
+
+# 1-a : Indexer la banque à l'aide de bowtie2-build
+./soft/bowtie2-build databases/all_genome.fasta results/rebuilt_genome/built_genome.fasta
 
 #./soft/bowtie2 -1 -2 -S results/sam_output/sam_out.sam
 
