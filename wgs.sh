e379ce1e91acd6c860f82647f3ee00dcb1460b2f
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-24 12:02:16 +0200

    ajout d'une condition pour la decompression.

--- wgs.sh
+++ wgs.sh
@@ -3,10 +3,15 @@
 echo "---- WGS ----"
 
 # Decompresser l'échantionage G
-ECHG_FILE=fastq/EchG_R1.fastq
-if test -f "$ECHG_FILE";then
+ECHG_R1_FILE=fastq/EchG_R1.fastq
+ECHG_R2_FILE=fastq/EchG_R2.fastq
+
+# Verification que les fichiers sont bien decompresser.
+if [ -f "$ECHG_R1_FILE" ] && [ -f "$ECHG_R2_FILE" ]
+then
     echo "Les fichier EchG_R1 et EchG_R2 sont décompressés."
 else
     gunzip fastq/EchG*.fastq.gz
 fi
+
 
